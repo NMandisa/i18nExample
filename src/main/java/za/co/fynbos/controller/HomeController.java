00a10b192bfd3123ce8647f117eb948d09490ca8
Noxolo Mandisa Mkhungo <noxolomandisamkhungo@gmail.com>
2024-04-20 23:50:55 +0200

    Web component i18n

--- src/main/java/za/co/fynbos/controller/HomeController.java
+++ src/main/java/za/co/fynbos/controller/HomeController.java
@@ -1,22 +1,38 @@
+/*
+ * Copyright (c) 2024.  Noxolo Mandisa Mkhungo
+ */
+
 package za.co.fynbos.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Locale;
 
 /**
  * @author Noxolo.Mkhungo
  */
+@Slf4j
 @Controller
 public class HomeController {
-    private ResourceBundleMessageSource messageSource;
-    public void setMessageSource(@Qualifier("webMessageSource") ResourceBundleMessageSource messageSource) {
-        this.messageSource = messageSource;
+    @SuppressWarnings("unsed")
+    private final ResourceBundleMessageSource bundleMessageSource;
+    @Autowired
+    public HomeController(@Qualifier("webMessageSource")ResourceBundleMessageSource bundleMessageSource){
+        this.bundleMessageSource=bundleMessageSource;
     }
-
-    @GetMapping
-    public String index(){
-        return "index";
+    @GetMapping("/")
+    public ModelAndView index(@RequestParam(name = "lang",required = false)String localParam){
+        Locale locale = Locale.getDefault();
+        if(localParam != null && !localParam.isEmpty()){locale= Locale.of(localParam);}
+        log.info("Welcome home! The client locale is {}.", locale);
+        return new ModelAndView("index");
     }
 }
